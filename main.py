3882f96775361e031fa94371f9aef5fa79d20d4a
himanshu98-aka <freelancinghimanshu@gmail.com>
2025-11-16 15:00:00 +0530

    v2

--- main.py
+++ main.py
@@ -12,16 +12,18 @@ if uploaded_files:
     for file in uploaded_files:
         img = Image.open(file).convert("RGB")
         images.append(img)
+    name = st.text_input("output file name :")
+    if name:
+        if st.button("Convert to PDF"):
+            pdf_bytes = io.BytesIO()
+            images[0].save(pdf_bytes, format="PDF", save_all=True, append_images=images[1:])
+            pdf_bytes.seek(0)
 
-    if st.button("Convert to PDF"):
-        pdf_bytes = io.BytesIO()
-        images[0].save(pdf_bytes, format="PDF", save_all=True, append_images=images[1:])
-        pdf_bytes.seek(0)
-
-        st.success("PDF generated successfully!")
-        st.download_button(
-            label="Download PDF",
-            data=pdf_bytes,
-            file_name="converted_images.pdf",
-            mime="application/pdf"
-        )
+            st.success("PDF generated successfully!")
+            
+            st.download_button(
+                label="Download PDF",
+                data=pdf_bytes,
+                file_name=f"{name}.pdf",
+                mime="application/pdf"
+            )
